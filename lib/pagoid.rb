1c5323cee259fd16860e58ed95c389e76f128e57
Jon Phenow <j.phenow@gmail.com>
2013-06-25 17:07:53 -0500

    load the objects

--- lib/pagoid.rb
+++ lib/pagoid.rb
@@ -1,5 +1,18 @@
 require "pagoid/version"
+begin
+  require 'kaminari'
+  require 'kaminari/models/array_extension'
+rescue LoadError
+  begin
+    require 'will_paginate'
+  rescue LoadError
+    raise LoadError, "Please install kaminari or will_paginate for Pagoid backends"
+  end
+end
+
+require 'pagoid/pager'
+require 'pagoid/controller_extensions'
+require 'pagoid/engine' if defined?(Rails)
 
 module Pagoid
-  # Your code goes here...
 end

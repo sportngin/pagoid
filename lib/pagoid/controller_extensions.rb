e282fd7ca815040bef67aa8aa0074efc62138168
Jon Phenow <j.phenow@gmail.com>
2013-06-25 17:06:49 -0500

    controller extensions

---
+++ lib/pagoid/controller_extensions.rb
@@ -0,0 +1,33 @@
+module Pagoid
+  module ControllerExtensions
+    def self.included(base)
+      base.extend ClassMethods
+    end
+
+    def paged(resource)
+      pager = Pager.new(resource, params)
+      self.class.apply_pager_blocks(self, pager)
+      pager.paginated
+    end
+
+    module ClassMethods
+      def when_paginated(&block)
+        @pagination_block = block
+      end
+
+      def apply_pager_blocks(context, pager_instance)
+        pager_blocks.each { |block| context.instance_exec(pager_instance, &block) }
+      end
+
+      def pager_block
+        @pagination_block || ->(x) { x }
+      end
+      private :pager_block
+
+      def pager_blocks
+        [pager_block] + (superclass.respond_to?(:pager_blocks) ? superclass.pager_blocks : [])
+      end
+      protected :pager_blocks
+    end
+  end
+end

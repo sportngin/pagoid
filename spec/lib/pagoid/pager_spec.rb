5defc400e740f4b54e5ce9d9aecaa27392105f68
Jon Phenow <j.phenow@gmail.com>
2013-06-27 10:44:08 -0500

    use AR in tests

--- spec/lib/pagoid/pager_spec.rb
+++ spec/lib/pagoid/pager_spec.rb
@@ -42,18 +42,13 @@ describe Pager do
   end
 
   describe "ActiveRecord paging" do
-    let(:active_record_class) { double }
-    let(:active_record_object) { double }
-
     before do
-      active_record_class.stub sort: active_record_class,
-        all: active_record_class,
-        first: active_record_object,
-        order: active_record_class,
-        offset: active_record_class
+      %w[Mike Pat Jon Adam].each do |name|
+        Person.create name: name
+      end
     end
 
-    let(:paginatable) { active_record_class }
+    let(:paginatable) { Person }
     let(:options) { {} }
     subject { described_class.new paginatable, options }
 
@@ -65,28 +60,28 @@ describe Pager do
       let(:per_page) { 2 }
       let(:page) { 1 }
 
-      its(:paginated) { should == active_record_class.first(2) }
+      its(:paginated) { should == Person.first(2) }
       its(:display_headers?) { should be_true }
 
       describe "sorting" do
-        let(:options) { { per_page: per_page, page: page, order_by: :first_name } }
+        let(:options) { { per_page: per_page, page: page, order_by: :name } }
 
-        its(:paginated) { should == active_record_class.order(options[:order_by]).first(2) }
+        its(:paginated) { should == Person.order(options[:order_by]).first(2) }
         its(:display_headers?) { should be_true }
 
         describe "with paging" do
           let(:page) { 2 }
 
           its(:paginated) {
-            should == active_record_class.offset(options[:per_page] * (page - 1)).order(options[:order_by]).first(2)
+            should == Person.offset(options[:per_page] * (page - 1)).order(options[:order_by]).first(2)
           }
           its(:display_headers?) { should be_true }
 
           describe "with direction" do
             let(:page) { nil }
-            let(:options) { { per_page: per_page, page: page, order_by: :first_name, direction: "asc" } }
+            let(:options) { { per_page: per_page, page: page, order_by: :name, direction: "asc" } }
 
-            its(:paginated) { should == active_record_class.order("first_name ASC").first(2) }
+            its(:paginated) { should == Person.order("name ASC").first(2) }
             its(:display_headers?) { should be_true }
           end
         end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,14 +4,18 @@
 # loaded once.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'active_record'
 require 'pagoid'
 root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 ActiveRecord::Base.establish_connection(
   adapter: "sqlite3",
   database: "#{root}/db/pagoid.db"
 )
+ActiveRecord::Base.connection.execute("DROP TABLE IF EXISTS 'people'")
 ActiveRecord::Base.connection.create_table(:people) do |t|
   t.string :name
+
+  t.timestamps
 end
 class Person < ActiveRecord::Base
 end

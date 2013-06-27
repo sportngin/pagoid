a3586084f2d9fcdd9dad2486381518d2b02c60a4
Jon Phenow <j.phenow@gmail.com>
2013-06-27 10:09:55 -0500

    setup a DB for testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,16 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require 'pagoid'
+root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+ActiveRecord::Base.establish_connection(
+  adapter: "sqlite3",
+  database: "#{root}/db/pagoid.db"
+)
+ActiveRecord::Base.connection.create_table(:people) do |t|
+  t.string :name
+end
+class Person < ActiveRecord::Base
+end
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true

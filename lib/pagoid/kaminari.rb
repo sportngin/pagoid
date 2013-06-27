a4c5a907ccf1c91b58b429fc540d7f9975289cba
Jon Phenow <j.phenow@gmail.com>
2013-06-27 09:07:58 -0500

    kaminari adapter

---
+++ lib/pagoid/kaminari.rb
@@ -0,0 +1,15 @@
+module Pagoid
+  class Kaminari < PagingAdapter
+    def coerce(coerceable = paginatable)
+      coerce?(coerceable) ? Kaminari.paginate_array(Array(coerceable)) : coerceable
+    end
+
+    def coerce?(coerceable = paginatable)
+      !(
+        coerceable.respond_to?(:order) &&
+        coerceable.respond_to?(:page) &&
+        coerceable.page.respond_to?(:per)
+      )
+    end
+  end
+end

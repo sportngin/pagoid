f46552c136f852b2a57f992dcad0339ca857e341
Jon Phenow <j.phenow@gmail.com>
2013-06-28 15:29:23 -0500

    attempt a page proxy object

--- lib/pagoid/kaminari.rb
+++ lib/pagoid/kaminari.rb
@@ -1,8 +1,15 @@
+require 'pagoid/paging_adapter'
 module Pagoid
   class Kaminari < PagingAdapter
     def coerce(coerceable = paginatable)
       coerce?(coerceable) ? ::Kaminari.paginate_array(Array(coerceable)) : coerceable
     end
+
+    def page(*)
+      PageProxy.new(super)
+    end
+
+    private
 
     def coerce?(coerceable = paginatable)
       !(
@@ -10,6 +17,12 @@ module Pagoid
         coerceable.respond_to?(:page) &&
         coerceable.page.respond_to?(:per)
       )
+    end
+
+    class PageProxy < SimpleDelegator
+      def per(*)
+        Kaminari.new super
+      end
     end
   end
 end
